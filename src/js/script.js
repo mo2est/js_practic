4b166e0eab033463dc7a0f8306d8d81ff05f46b9
Andrew Savinov <savinov300793@gmail.com>
2023-12-18 23:42:40 +0400

    practic with obj and arr

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,26 @@ console.log(obj.userName, obj.age, obj.isMarried, obj.wifeName, obj.isChild, obj
 const arr = [1, 'Andrew', 'Irina', 'Michail', 3];
 console.log(arr[3]);
 
+/* /////////////////////////// */
+/* 18/12/2023 */
+/* /////////////////////////// */
+
+
+/* Object */
+const shopObj = {
+    tomatoes: 5,
+    bread: 3,
+    cheese: 5
+};
+
+shopObj.apple = 10;
+
+console.log(shopObj['apple']);
+console.log(shopObj.apple)
+
+
+/* Array */
+const varArr = [1, 2, 3];
+varArr[3] = 4;
+
+console.log(varArr[0]);
