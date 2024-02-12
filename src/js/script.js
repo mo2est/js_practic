ca07e760cb12619aedbb8a0a049aebf8bb5a520e
Andrew Savinov <savinov300793@gmail.com>
2024-02-12 23:05:05 +0400

    first homework done 12/02/2024

--- src/js/script.js
+++ src/js/script.js
@@ -172,3 +172,109 @@ const nuggets = 2;
 if (hamburger && cola || fries === 3 && nuggets) {
    console.log('Done!')
 }
+
+/////циклы/////////////////////
+
+let num = 50;
+
+/* while (num <= 55) {
+    console.log(num);
+    num++;
+} */
+
+/* do {
+    console.log(num);
+    num++;
+}
+
+while(num <= 55); */
+
+/* for (let i = 1; i <= 8; i++) {
+    console.log(num);
+    num++;
+}; */
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 6) {
+        /* break; */
+        continue;
+    }
+    console.log(i);
+};
+
+/////////////////////циклы в циклах///////////
+let result = '';
+const length = 7;
+
+for (i = 1; i < length; i++) {
+
+    for (j = 0; j < i; j++) {
+        result += '*';
+    }
+
+    result += '\n';
+}
+
+console.log(result);
+
+first: for (i = 0; i < 3; i++) {
+    console.log(`Первый этап: ${i}`);
+    for (j = 0; j < 3; j++) {
+        console.log(`Второй этап: ${j}`);
+        for (k = 0; k < 3; k++) {
+            if (k === 1) continue first;
+            console.log(`Третий этап: ${k}`);
+        }
+    }
+};
+
+////////home work/////////////////
+
+let num = 5;
+
+while (num <= 10) {
+    console.log(num);
+    num++;
+};
+///////////////////////////////
+for(let i = 21; i--;) {
+    if (i === 13) break;
+    console.log(i);
+};
+////
+for (let i = 20; i >= 10; i--) {
+        if (i === 13) break;
+        console.log(i)
+    }
+///////////////////////////////
+for (let i = 2; i<=10; i++) {
+    console.log(i);
+    i++;
+}
+///////////////////////////////
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+///while var/////////////////
+let i = 2;
+while (i < 16) {
+    i++;
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+};
+//////////////////////////
+ 
+ const arrayOfNumbers = [];
+
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
